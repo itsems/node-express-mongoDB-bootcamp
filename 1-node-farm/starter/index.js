24b5ba4c3ecf89c53b6abfba4de9bbaff492b570
Emma <emma801006@hotmail.com>
2022-05-18 16:58:05 +0800

    write my first api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const http = require('http');
+const url = require('url');
+
+// Files
 
 // Blocking, sync way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -7,7 +11,32 @@ const fs = require('fs')
 // fs.writeFileSync('./txt/output.txt', textOut)
 
 // Non-blocking, async way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// })
+// console.log('will read file!');
+
+// Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+  
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/')
+    res.end('hello from server')
+  else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json'});
+    res.end(data)
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello emma'
+    });
+    res.end('<h1>Page not found!</h1>')
+  }
 })
-console.log('will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening on 8000');
+})
