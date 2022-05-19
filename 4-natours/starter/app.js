d749e716eb5596a8b466b58954c8b93f7accc5eb
Emma <emma801006@hotmail.com>
2022-05-20 00:12:03 +0800

    get with params & patch & delete

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -25,6 +25,28 @@ app.get('/api/v1/tours',(req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id',(req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; // conver string to number
+  const tour = tours.find(el => el.id === id)
+
+  // if(id > tours.length) {
+  if(!tour) {
+    return res.status(404).json({ 
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(200).json({
+    status: 'ok',
+    // results: tours.length,
+    data: {
+      tour 
+    }
+  })
+})
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1 ].id + 1;
@@ -41,6 +63,43 @@ app.post('/api/v1/tours', (req, res) => {
     })
   })
 })
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  
+  if(req.params.id * 1 > tours.length) {
+      return res.status(404).json({ 
+        status: 'fail',
+        message: 'Invalid ID'
+      })
+    }
+
+  res.status(200).json({
+    status: 'ok',
+    data: { 
+      tour: '<Updataed tour here...>'
+    }
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  if(req.params.id * 1 > tours.length) {
+      return res.status(404).json({ 
+        status: 'fail',
+        message: 'Invalid ID'
+      })
+    }
+
+  res.status(204).json({
+    status: 'ok',
+    data: null
+  })
+})
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
