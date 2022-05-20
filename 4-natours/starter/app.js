b95cf1a5c03240d4fb557bcc974e6497e8279883
Emma <emma801006@hotmail.com>
2022-05-20 16:46:18 +0800

    env

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,7 +6,11 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
-app.use(morgan('dev'));
+console.log('NODE_ENV', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
