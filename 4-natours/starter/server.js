ca7fa0191ee3a496f267c4de0c682de88870f977
Emma <emma801006@hotmail.com>
2022-05-22 21:45:48 +0800

    create document success

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -14,6 +14,39 @@ mongoose
     useFindAndModify: false
   }).then(() => console.log('db connection successful!'))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 999
+})
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  }).catch(err => {
+    console.log('error', err);
+  })
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
