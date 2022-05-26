faa71738d571a14fa9807f0e5fa98748d9d6675e
Emma <emma801006@hotmail.com>
2022-05-26 22:01:22 +0800

    Reading Document

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,70 +2,82 @@ const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   console.log('check body has name and price property', req.body);
-  if (!req.body.name || !req.body.price){
+  if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price'
-    })
+      message: 'Missing name or price',
+    });
   }
   next();
-}
+};
 
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
+    res.status(200).json({
+      status: 'ok',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    res.status(200).json({
+      status: 'ok',
+      data: { tour }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
 
-  res.status(200).json({
-    status: 'ok',
-    requestAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours 
-    // }
-  })
-}
-
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; // conver string to number
-  
   // const tour = tours.find(el => el.id === id)
 
   // res.status(200).json({
   //   status: 'ok',
   //   // results: tours.length,
   //   data: {
-  //     tour 
+  //     tour
   //   }
   // })
-}
+};
 
 exports.createTour = (req, res) => {
-  
   res.status(201).json({
     status: 'ok',
     // data: {
     //   tour: newTour
     // }
-  })
-}
+  });
+};
 
 exports.updateTour = (req, res) => {
   console.log(req.params);
   res.status(200).json({
     status: 'ok',
-    data: { 
-      tour: '<Updataed tour here...>'
-    }
-  })
-}
+    data: {
+      tour: '<Updataed tour here...>',
+    },
+  });
+};
 
 exports.deleteTour = (req, res) => {
   console.log(req.params);
   res.status(204).json({
     status: 'ok',
-    data: null
-  })
-}
-
+    data: null,
+  });
+};
