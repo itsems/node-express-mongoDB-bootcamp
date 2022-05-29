da521254b8f823cb7660f35a69deb6d17fb74713
Emma <emma801006@hotmail.com>
2022-05-29 19:52:03 +0800

    Query middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -58,6 +58,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -85,7 +89,19 @@ tourSchema.pre('save', function(next) {
 //   next();
 // })
 
-// 
+// Query middleware
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true }})
+  this.start = Date.now()
+  next();
+})
+
+// after query execute
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
