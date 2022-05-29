a1c8d37cda9da517b26e034a198d793be5980088
Emma <emma801006@hotmail.com>
2022-05-29 20:53:40 +0800

    validator

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,9 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 40 characters'],
+      validate: [validator.isAlpha, 'Tour name must only caontain characters']
     },
     slug: String,
     duration: {
@@ -20,10 +24,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'mediumn', 'difficult'],
+        messaeg: 'Difficaulty is either: easy, medium, difficault'
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'], 
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -37,7 +47,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate:{
+          // this only points to current doc on NEW document creation
+          validator: function(val) {
+          return val < this.price;
+        },
+        message: 'discount 要比較便宜，({VALUE}) 太貴'
+      }
+    },
     summary: {
       type: String,
       trim: true,
@@ -106,8 +125,6 @@ tourSchema.post(/^find/, function(docs, next) {
 // Aggregation middleware
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({$match: { secretTour: { $ne: true } } })
-
-
   console.log(this.pipeline()); // current agg obj
   next();
 })
