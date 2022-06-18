c0860a7b09d4e4df3827aea36b3f587c9da98589
Emma <emma801006@hotmail.com>
2022-06-18 22:56:38 +0800

    Uncaught Exception

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('Unhandled Exception! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
