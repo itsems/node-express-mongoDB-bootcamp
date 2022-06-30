2b284d7e4662ae47bafa2be7372ab5af8da56aed
Emma <emma801006@hotmail.com>
2022-06-30 17:59:15 +0800

    Loggin in Users

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,34 +1,43 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'not yet'
-  })
-}
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // Send Response
+  res.status(200).json({
+    status: 'ok',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'not yet'
-  })
-}
+    message: 'not yet',
+  });
+};
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'not yet'
-  })
-}
+    message: 'not yet',
+  });
+};
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'not yet'
-  })
-}
+    message: 'not yet',
+  });
+};
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'not yet'
-  })
-}
+    message: 'not yet',
+  });
+};
